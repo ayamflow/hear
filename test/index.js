9235afdce4a0253fb6bab92fc9c868b5c93144c9
Florian Morel <ayamflow@gmail.com>
2014-12-24 19:48:20 +0100

    Removed todo, all tests done

--- test/index.js
+++ test/index.js
@@ -3,12 +3,3 @@
 require('./dom');
 require('./event-emitter');
 require('./mediator');
-
-/*
-    TODO
-    - test on √
-    - test off √
-    - test once √
-    - test with arguments on emit √
-    - test all with context
- */
